d76f22ce10420291840a8a896135a38017b91fd5
Alexis <alexis@bootstragram.com>
2015-07-20 18:13:00 +0200

    Fix issue#1

--- bin/prebuild.rb
+++ bin/prebuild.rb
@@ -9,7 +9,11 @@ if Dir.exist? 'resources/images/'
   Dir.foreach('resources/images') do |filename|
     ext = File.extname(filename);
     if formats.include? ext
-      json['resources']['media'] << {type: ext[1..-1], file: "images/#{filename}", name: "IMAGE_#{filename[0..-(ext.length + 1)].upcase}"}
+      json['resources']['media'] << {
+          type: ext[1..-1],
+          file: "images/#{filename}",
+          name: "IMAGE_#{filename[0..-(ext.length + 1)].gsub('/[a-z]/', 'LOL').gsub(/\W/i, '_').upcase}"
+      }
     end
   end
   File.write('appinfo.json', JSON.pretty_generate(json))
