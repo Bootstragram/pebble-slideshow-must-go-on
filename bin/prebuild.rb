7690f9c22f3e5adceb4429035d96bf220c71eff1
Alexis <alexis@bootstragram.com>
2015-07-21 09:15:43 +0200

    Add managing of multiplatform images files (example~color.png)

--- bin/prebuild.rb
+++ bin/prebuild.rb
@@ -6,13 +6,21 @@ if Dir.exist? 'resources/images/' and File.file? 'appinfo.json'
   json = JSON.parse(File.read('appinfo.json'))
   json['resources']['media'] = []
   formats = ['.png', '.jpg', '.jpeg', 'bmp']
+  platformFiles = [];
   Dir.foreach('resources/images') do |filename|
     ext = File.extname(filename);
     if formats.include? ext
+      if filename.include? '~'
+        filename = filename.sub(/(\w)~.+/, '\1') + ext
+        if platformFiles.include? filename
+          next
+        end
+        platformFiles << filename
+      end
       json['resources']['media'] << {
           type: ext[1..-1],
           file: "images/#{filename}",
-          name: "IMAGE_#{filename[0..-(ext.length + 1)].gsub('/[a-z]/', 'LOL').gsub(/\W/i, '_').upcase}"
+          name: "IMAGE_#{filename[0..-(ext.length + 1)].gsub(/\W/i, '_').upcase}"
       }
     end
   end
